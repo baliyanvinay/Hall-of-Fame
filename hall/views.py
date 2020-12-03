4d271f63fc8e35129336605536fa31c211cc276a
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-12-03 18:05:32 +0530

    New User creation nows enables auto login

--- hall/views.py
+++ hall/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 # Create your views here.
@@ -13,6 +14,15 @@ class SignUp(generic.CreateView):
     success_url=reverse_lazy('home')
     template_name='registration/signup.html'
 
+    def form_valid(self, form):
+        # Automatic login upon signup
+        view=super(SignUp, self).form_valid(form) # Getting view from super call
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user=authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+
 class CreateHall(generic.CreateView):
     model=Hall
     fields=['title']
@@ -22,5 +32,5 @@ class CreateHall(generic.CreateView):
     def form_valid(self, form):
         # Assigning self.request.user to form instance user
         form.instance.user=self.request.user
-        super(CreateHall, self).form_valid(form) # called super for remaining operation of 
+        super(CreateHall, self).form_valid(form) # called super for remaining operation
         return redirect('home')
